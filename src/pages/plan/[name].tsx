bc2f97c740e69f2cd7cf2314be972c9106dc2829
Riley <riley@rileybathurst.com>
2022-11-01 12:00:38 -0700

    catch all problem

--- src/pages/plan/[name].tsx
+++ src/pages/plan/[name].tsx
@@ -5,6 +5,8 @@ function PlanCatchAll({ params }) {
     <>
       <h1>Plan Catch All</h1>
       <p>{params.name}</p>
+      <p>This plan does not exist</p>
+      <p>14245-e-windriver-ln suffered this fate - possibly because of a mess up letween capital I and lowercase l</p>
     </>
   )
 }
