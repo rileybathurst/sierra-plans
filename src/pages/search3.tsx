011621de8f00b5e609b5d198b6687c2202f733bd
Riley <riley@rileybathurst.com>
2023-01-13 14:41:44 -0800

    back to working go to 7

---
+++ src/pages/search3.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Search6 from "../components/search6"
+
+const Search3Page = () => {
+  return (
+    <main>
+      <h1>Search Test</h1>
+      <Search6 />
+    </main>
+  )
+}
+
+export default Search3Page
