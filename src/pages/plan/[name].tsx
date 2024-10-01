c0b2d8d0792382eb3ecbeefb2cd78bc404e54a64
Riley <riley@rileybathurst.com>
2024-10-01 10:15:55 -0700

    Refactor plan/[name].tsx to add home link

--- src/pages/plan/[name].tsx
+++ src/pages/plan/[name].tsx
@@ -1,12 +1,14 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
-function PlanCatchAll({ params }) {
+function PlanCatchAll({ params }: { params: { name: string } }) {
   return (
     <>
       <h1>Plan Catch All</h1>
       <p>{params.name}</p>
       <p>This plan does not exist</p>
       <p>14245-e-windriver-ln suffered this fate - possibly because of a mess up letween capital I and lowercase l</p>
+      <Link to="/">Home</Link>
     </>
   )
 }
